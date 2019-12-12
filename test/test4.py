246ce250c07884bbd67b4f71441fb7e37e54a339
ho <ho@localhost.localdomain>
2019-12-12 03:55:04 -0500

    Add test4.py

--- test/test4.py
+++ test/test4.py
@@ -4,15 +4,14 @@ import sys
 input_ea=0;
 
 #conn = pymysql.connect(host='localhost', user='root', password='123', charset='utf8mb4')
-sql1 = 'select * from study_test.test1' #test1 출력
-sql2 = "insert into study_test.test1(Date, Note, Count, Ea)values(%s, %s, %s, %s)"#test1 입력
-sql3 = "insert into study_test.EA_table(EA)values(%s)"  #티켓 수량 입력
-sql4 = 'select * from study_test.test1 where Ea OR NULL' #값이 있는 것 만 출력
-sql5 = "insert into study_test.test1(Ea)values(%s)"#test1 입력
+sql1 = 'select * from study.information' #test1 출력
+sql2 = "insert into study.information(Date, Note, Count)values(%s, %s, %s)"#test1 입력
+sql3 = "insert into study.information(EA)values(%s)"  #티켓 수량 입력
+sql4 = 'select * from study.information where Ea OR NULL' #값이 있는 것 만 출력
+sql5 = "insert into study.information(Ea)values(%s)"#test1 입력
 
 def select():
     conn = pymysql.connect(host='localhost', user='root', password='123', charset='utf8mb4')
-
     try:
         with conn.cursor() as cursor:
            # sql1 = 'select * from study_test.test1'
@@ -30,11 +29,11 @@ def insert(input_Date, input_Note, input_Count):
     try:
         with conn.cursor() as cursor:
             #sql2 = "insert into study_test.test1(Date, Note, Count)values(%s, %s, %s)"
-            if(input_ea <= 0):
-                cursor.execute(sql2, (input_Date, input_Note, input_Count, input_ea))
-            else:
-                input_ea = input_ea - input_Count
-                cursor.execute(sql2, (input_Date, input_Note, input_Count, input_ea))
+            #if(input_ea <= 0):
+            cursor.execute(sql2, (input_Date, input_Note, input_Count))
+            #else:
+             #   input_ea = input_ea - input_Count
+             #   cursor.execute(sql2, (input_Date, input_Note, input_Count, input_ea))
             #cursor.execute(sql5, (input_ea))
             conn.commit()
     finally:
